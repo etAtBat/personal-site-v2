a98426619b2e5eddd85f02fca5213cd425fa9ef5
Egan Tamashiro <egantamashiro@gmail.com>
2016-03-20 22:28:42 -0700

    adds transitions to navigation bar

--- client/js/main.js
+++ client/js/main.js
@@ -1,30 +1,27 @@
 $(document).ready(function () {
 
-  console.log('jQuery online');
-  
   /**********
     functionality for the navigation bar
   ***********/
   
-  $('.topnav').on('mouseover', function () {
-    $(this).toggleClass('active', '')
-  });
+  // $('.topnav').on('mouseover', function () {
+  //   $(this).toggleClass('selected', '')
+  // });
 
-  $('.topnav').on('mouseout', function () {
-    $(this).toggleClass('active', '');
-  });
+  // $('.topnav').on('mouseout', function () {
+  //   $(this).toggleClass('selected', '');
+  // });
 
   //scrolling to any part of the page from navigation bar
   var navHeight = $(".navbar-header").height(); 
 
   $('.topnav a').click(function(){
-    console.log($($(this).attr('href')));
-    console.log($($(this).attr('href')).offset().top)
+    // console.log($($(this).attr('href')));
+    // console.log($($(this).attr('href')).offset().top)
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top - navHeight
     }, 500);
     return false;
   });
-
 
 });
